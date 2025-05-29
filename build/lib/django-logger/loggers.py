94d9edaeaa4886ebb49f40e0f5fc99e1618424f5
MohammadMahdiNiliSani <m.m.nilisani1388@gmail.com>
2025-05-29 17:43:04 +0330

    #1 ایجاد دوباره ی whl

--- build/lib/django-logger/loggers.py
+++
@@ -1,8 +0,0 @@
-loggers = {
-    'django-logger': {
-        'handlers': ['console', 'rotating_file_handler_info', 'rotating_file_handler_error',
-                     'rotating_file_handler_critical'],
-        'level': 'INFO',
-        'propagate': False,
-    },
-}
