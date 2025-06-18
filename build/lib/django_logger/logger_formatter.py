b271c957705243cd3c89920ab29377926e95b29f
MahdiarMohammadi <mahdiarmohammadi7@gmail.com>
2025-06-18 15:58:30 +0330

    #1 اصلاح مشکلات لاگر

--- build/lib/django_logger/logger_formatter.py
+++ build/lib/django_logger/logger_formatter.py
@@ -16,12 +16,26 @@ class JalaliJsonFormatter(JsonFormatter):
 formatter = {
     'standard': {
         '()': 'django_logger.logger_formatter.JalaliFormatter',
-        'format': '\nlogged:\n\t{message} - {status_code} - {url} - {method}\n\tuser: {user}\n\trequest data: {request_data}\n\tresponse data: {response_data}\n\terror: {error_name}\n\t{asctime} - {levelname} - {module_name}\n',
+        'format': (
+            '\nlogged:\n\t{message} - {status_code} - {url} - {path} - {method}\n\t'
+            'user: {user}\n\tIP: {IP}\n\tSession Id: {session_id}\n\tProtocol: {Protocol}\n\t'
+            'request data: {request_data}\n\tresponse data: {response_data}\n\t'
+            'Sec Ch Ua Platform: {sec_ch_ua_platform}\n\tUser Agent: {user_agent}\n\t'
+            'Accept Encoding: {accept_encoding}\n\tCsrf Token: {csrf_token}\n\t'
+            'error: {error_name}\n\t{asctime} - {levelname} - {module_name}\n'
+        ),
         'style': '{',
     },
     'json': {
         '()': 'django_logger.logger_formatter.JalaliJsonFormatter',
-        'fmt': '{message} - {status_code} - {url} - {method} - {user} - {request_data} - {response_data} - {error_name} - {asctime} - {levelname} - {module_name}',
+        'fmt': (
+            '{message} - {status_code} - {url} - {path} - {method} - '
+            '{user} - {IP} - {session_id} - {Protocol} - '
+            '{request_data} - {response_data} - '
+            '{sec_ch_ua_platform} - {user_agent} - '
+            '{accept_encoding} - {csrf_token} - '
+            '{error_name} - {asctime} - {levelname} - {module_name}'
+        ),
         'style': '{'
     },
 }
